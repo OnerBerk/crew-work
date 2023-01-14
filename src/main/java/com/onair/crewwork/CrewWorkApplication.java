4348554f57f759b740ed4ec7a0ff0fdb90e62b26
OnerBerk <oner.berk@joza-it.fr>
2023-01-14 12:23:33 +0100

    initial commit

---
+++ src/main/java/com/onair/crewwork/CrewWorkApplication.java
@@ -0,0 +1,13 @@
+package com.onair.crewwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onair/crewwork/CrewWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onair.crewwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
